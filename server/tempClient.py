608a5efaa44854152e2ea283027d20ca29e76751
numberking123 <clemens.mosig@gmail.com>
2019-05-19 08:22:44 +0200

    temp client

---
+++ server/tempClient.py
@@ -0,0 +1,6 @@
+import urllib.request
+
+#f = urllib.request.urlopen('http://192.168.43.7:8081/eval?url=http://google.com&text=Fakenewsisreal&requestID=12345')
+#f = urllib.request.urlopen('http://192.168.43.7:8081/eval?url=https://www.activistpost.com/&text=Love&requestID=18366487278223')
+f = urllib.request.urlopen('http://192.168.43.7:8081/eval?url=http://bbsamplebb.q1.com&text=Love&requestID=18366487278223')
+print(f.read())
